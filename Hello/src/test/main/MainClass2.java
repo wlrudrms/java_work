c72a7bfcca317c61402f7413f7a658c8f2b5f9f0
kyungkeun <wlrudrms19@naver.com>
2022-10-25 11:04:38 +0900

    Hello 프로젝트 추가

---
+++ Hello/src/test/main/MainClass2.java
@@ -0,0 +1,21 @@
+// 이 클래스가 속해있는 패키지명을 밝혀줘야한다. ;반드시 필요
+package test.main;
+/*
+ * 여러줄 주석입니다.
+ * 하나
+ * 두울
+ * 세엣
+ */
+
+//공개된 클래스(모든 패키지에서 사용할 수 있는) 클래스 , 클래스명은 MainClass2 
+public class MainClass2 { //클래스의 시작
+	//한줄 주석입니다.
+	/*
+	 *  메소드명 : main   (ctrl + space)
+	 *  run 했을 때 실행이 시작되는 아주 특별한 메소드
+	 */
+	public static void main(String[] args) {
+		
+	}
+	
+} //클래스의 끝
