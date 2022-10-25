705fbc9f02c051a5d9227fc328df491c29bf921f
kyungkeun <wlrudrms19@naver.com>
2022-10-25 15:39:26 +0900

    Step04_Method 예제 추가

--- Step04_Method/src/test/main/MainClass01.java
+++ Step04_Method/src/test/main/MainClass01.java
@@ -1,5 +1,6 @@
 package test.main;
 
+import test.mypac.Car;
 import test.mypac.MyObject;
 
 public class MainClass01 {
@@ -12,5 +13,23 @@ public class MainClass01 {
 		o1.walk();
 		int num = o1.getNumber();
 		String greet = o1.getGreeting();
+		Car car = o1.getCar();
+		// setNum(), setName(), useCar() 메소드를 호출해보세요
+
+		// 메소드를 호출하면서 인자로 전달할 데이터를 즉석에서 만들어서 전달하기
+		o1.setNum(1);
+		o1.setName("김구라");
+		o1.useCar(new Car());
+
+		// 전달할 값이 들어있는 변수명으로 전달할 수 있다.
+		int a = 999;
+		String b = "누구게";
+		Car c1 = new Car();
+		o1.setNum(a);
+		o1.setName(b);
+		o1.useCar(c1);
+
+		// doSomething() 메소드를 호출해보기
+		o1.doSomething(a, b, c1);
 	}
 }
--- Step04_Method/src/test/mypac/MyObject.java
+++ Step04_Method/src/test/mypac/MyObject.java
@@ -21,5 +21,25 @@ public class MyObject {
 
 	public String getGreeting() {
 		return "안녕하세요";
+	}	
+	//Car type을 리턴해주는 메소드
+	public Car getCar() {
+		return new Car();
+	}
+	//int type을 메소드의 인자로 전달받는 메소드
+	public void setNum(int num) {
+		System.out.println("num:"+num);
+	}
+	//String type을 메소드의 인자로 전달받는 메소드
+	public void setName(String name) {
+		System.out.println("name:"+name);
+	}
+	//Car type을 메소드의 인자로 전달받는 메소드
+	public void useCar(Car car) {
+		car.drive();
+	}
+	//3개의 type을 메소드의 인자로 전달받는 메소드
+	public void doSomething(int num, String msg, Car c) {
+		System.out.println("무언가를 작업합니다.");
 	}
 }
