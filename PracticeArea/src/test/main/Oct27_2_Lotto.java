b18c331d854083a9d9643d143be5b5a9e5830ab7
kyungkeun <wlrudrms19@naver.com>
2022-10-28 11:42:06 +0900

    PracticeArea 프로젝트에 Lotto문제 당첨루프추가

--- PracticeArea/src/test/main/Oct27_2_Lotto.java
+++ PracticeArea/src/test/main/Oct27_2_Lotto.java
@@ -4,56 +4,61 @@ import java.util.Random;
 
 public class Oct27_2_Lotto {
 	public static void main(String[] args) {
-		// 로또번호 생성
-		Random ran = new Random();
-		int[] myN = new int[6];
+		// 당첨될 때까지 돌리기
+		for (int k = 0; k < 10000000; k++) {
 
-		for (int i = 0; i < 6; i++) {
-			int r = ran.nextInt(1, 46);
-			myN[i] = r;
-			for (int j = 0; j < i; j++) {
-				if (myN[i] == myN[j]) {
-					i--;
-					break;
+			// 로또번호 생성
+			Random ran = new Random();
+			int[] myN = new int[6];
+
+			for (int i = 0; i < 6; i++) {
+				int r = ran.nextInt(1, 46);
+				myN[i] = r;
+				for (int j = 0; j < i; j++) {
+					if (myN[i] == myN[j]) {
+						i--;
+						break;
+					}
+				}
+			}
+			for (int i = 0; i < 6; i++) {
+				System.out.print(myN[i] + " ");
+			}
+
+			System.out.println();
+
+			// 당첨번호 생성
+			int[] lotto = new int[6];
+
+			for (int i = 0; i < 6; i++) {
+				int r = ran.nextInt(1, 46);
+				lotto[i] = r;
+				for (int j = 0; j < i; j++) {
+					if (lotto[i] == lotto[j]) {
+						i--;
+						break;
+					}
+				}
+			}
+			for (int i = 0; i < 6; i++) {
+				System.out.print(lotto[i] + " ");
+			}
+
+			System.out.println();
+
+			// 비교
+			int num = 0;
+			for (int i = 0; i < 6; i++) {
+				for (int j = 0; j < 6; j++) {
+					if (myN[i] == lotto[j]) {
+						num++;
+						if (num == 6) {
+							System.out.println("당첨!");
+							return;
+						}
+					}
 				}
 			}
 		}
-		for (int i = 0; i < 6; i++) {
-			System.out.print(myN[i] + " ");
-		}
-
-		System.out.println();
-
-		// 당첨번호 생성
-		int[] lotto = new int[6];
-
-		for (int i = 0; i < 6; i++) {
-			int r = ran.nextInt(1, 46);
-			lotto[i] = r;
-			for (int j = 0; j < i; j++) {
-				if (lotto[i] == lotto[j]) {
-					i--;
-					break;
-				}
-			}
-		}
-		for (int i = 0; i < 6; i++) {
-			System.out.print(lotto[i] + " ");
-		}
-		
-		System.out.println();
-
-		// 비교
-		int num = 0;
-		for (int i = 0; i < 6; i++) {
-			for (int j = 0; j < 6; j++) {
-				if(myN[i]==lotto[j]) {
-					num++;
-				}
-			}
-		}
-		
-		System.out.println(num);
-		
 	}
 }
