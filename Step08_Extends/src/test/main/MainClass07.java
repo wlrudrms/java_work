51f2b1abc7deb3ce0d2606165ade03f1efb32478
지경근 <wlrudrms19@naver.com>
2022-11-04 09:31:47 +0900

    ~Step08 복습 보충설명 추가

--- Step08_Extends/src/test/main/MainClass07.java
+++ Step08_Extends/src/test/main/MainClass07.java
@@ -5,7 +5,7 @@ import test.auto.Engine;
 
 public class MainClass07 {
 	public static void main(String[] args) {
-
+		// 생성자의 매개변수로 Engine객체를 전달하면서 ElectricCar객체 생성
 		ElectricCar car1 = new ElectricCar(new Engine());
 		car1.drive();
 		car1.charge();
