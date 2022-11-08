c41b57e19bbadcdcff768052581982c2f632fccf
지경근 <wlrudrms19@naver.com>
2022-11-08 16:07:39 +0900

    곱셈 중복제거

--- Algorithm/src/boj/main/Main_BOJ_2588_곱셈_b3.java
+++ Algorithm/src/boj/main/Main_BOJ_2588_곱셈_b3.java
@@ -2,15 +2,25 @@ package boj.main;
 
 import java.util.*;
 
-public class Main_BOJ_2588_곱셈_b3 {
-	public static void main(String[] args) {
-		
-		Scanner sc1 = new Scanner(System.in);
-		Scanner sc2 = new Scanner(System.in);
-		
-		//문자열 입력받기
-		int str1 = sc1.nextInt();
-		int str2 = sc2.nextInt();
-		
-	}
-}
+ public class Main_BOJ_2588_곱셈_b3 {
+ 	public static void main(String[] args) {
+ 		
+ 		Scanner scan = new Scanner(System.in);
+ 		
+ 		int num1 = scan.nextInt();
+ 		int num2 = scan.nextInt();
+ 		
+ 		int num2_10 = num2%100;
+ 		int num2_1 = num2_10%10;
+ 		int num2_real10 =(num2_10 - num2_1)/10;
+ 		int a = num2/100;
+ 		
+ 		
+ 		System.out.println(num1*num2_1);
+ 		System.out.println(num1*num2_real10);
+ 		System.out.println(num1*a);
+ 		
+ 		int c = num1*num2_1 + num1*num2_real10*10 + num1*a*100;
+ 		System.out.println(c);
+ 	}
+ }
